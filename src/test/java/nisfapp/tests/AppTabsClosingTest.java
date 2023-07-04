4854f9ba96f5c912219f7ff91d5c7fffa2e107af
Maksim T <roronoazorroippo@gmail.com>
2023-07-04 21:59:10 +0400

    disable test

--- src/test/java/nisfapp/tests/AppTabsClosingTest.java
+++ src/test/java/nisfapp/tests/AppTabsClosingTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class AppTabsClosingTest extends BaseTest {
 
 
-    @Test
+    @Test(enabled = false)
     public void closeAllAppUITabs() {
         doSFLogIn(SF_URL, SALES_OFFICER_USER);
         applicationPage.closeAllSFTabs();
